c094a0eb47f3f67b34db54e6a77721c172fd5340
Lars <larsbeijaard@outlook.com>
2020-09-08 21:12:08 +0200

    Sample !ping command

---
+++ Poller/Modules/TestModule.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Poller.Modules
+{
+    public class TestModule : ModuleBase<SocketCommandContext>
+    {
+        // Sample command
+        [Command("ping")]
+        public Task PingAsync()
+            => ReplyAsync("pong!");
+    }
+}
